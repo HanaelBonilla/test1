1d85ffd86ad6020041b0e4bdebc1587df94b27bd
HanaelBonilla <hanael0816@gmail.com>
2017-07-07 16:22:14 -0600

    Test
    test

--- src/Testng/Automation.java
+++ src/Testng/Automation.java
@@ -1,10 +1,15 @@
 package Testng;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -18,11 +23,18 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Automation {
 
 	public class yourLogoClass {
-	WebDriver driver = new ChromeDriver();
+	//WebDriver driver = new ChromeDriver();
+	WebDriver driver = new FirefoxDriver();
 	WebElement dynamicElement;
 	public WebDriverWait wait = new WebDriverWait(driver, 10);
 
@@ -30,14 +42,14 @@ public class Automation {
 public void beforeTesting(){
 	}
 	
-	@Test
+	/*@Test
 public void seleniumTesting(){
 	driver.get("http://automationpractice.com/");
 	driver.manage().window().maximize();
 	String title = driver.getTitle();
 	Assert.assertTrue(title.contains("My Store"));
 	driver.findElement(By.className("login")).click();
-	driver.findElement(By.id("email_create")).sendKeys("hanael@test1.com");
+	driver.findElement(By.id("email_create")).sendKeys("hanael@test2.com");
 	driver.findElement(By.id("SubmitCreate")).click();
 	title = driver.getTitle();
 	Assert.assertTrue(title.contains("Login - My Store"));
@@ -48,7 +60,7 @@ public void seleniumTesting(){
 	WebElement element = driver.findElement(By.id("email"));
 	String email = element.getAttribute("value");
 	System.out.println("Su email es" + email);
-	Assert.assertEquals("hanael@test1.com", email);
+	Assert.assertEquals("hanael@test2.com", email);
 	driver.findElement(By.id("passwd")).sendKeys("Test123");
 	Select dropdown = new Select(driver.findElement(By.id("days")));
 	dropdown.selectByIndex(2);
@@ -84,6 +96,32 @@ public void seleniumTesting(){
 	title = driver.getTitle();
 	Assert.assertTrue(title.contains("My account - My Store"));
 	
+	}*/
+	
+	@Test
+public void shoppingCart(){
+		driver.get("http://automationpractice.com/");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("My Store"));
+		driver.findElement(By.cssSelector("a[href*='http://automationpractice.com/index.php?controller=order']")).click();
+		WebElement test = wait.until(ExpectedConditions.elementToBeClickable(By.className("sf-with-ul")));
+		driver.findElement(By.className("sf-with-ul")).click();
+		test = wait.until(ExpectedConditions.elementToBeClickable(By.className("color_pick")));
+		driver.findElement(By.cssSelector(".product-image-container")).click();
+		//driver.findElement(By.cssSelector("a[href*='http://automationpractice.com/index.php?controller=cart&add=1&id_product=2&token=e817bb0705dd58da8db074c69f729fd8']")).click();
+		driver.findElement(By.cssSelector("a[href*='http://automationpractice.com/index.php?controller=order']")).click();
+		driver.findElement(By.cssSelector("a[href*='http://automationpractice.com/index.php?controller=order']")).click();
+		test = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".cart_navigation .button")));
+		driver.findElement(By.cssSelector("a[href*='http://automationpractice.com/index.php?controller=order&step=1']")).click();
+		driver.findElement(By.id("email")).sendKeys("hanael@test2.com");
+		driver.findElement(By.id("passwd")).sendKeys("Test123");
+		driver.findElement(By.id("SubmitLogin")).click();
+		driver.findElement(By.cssSelector(".cart_navigation .button")).click();
+		driver.findElement(By.id("cgv")).click();
+		driver.findElement(By.cssSelector(".cart_navigation .button")).click();
+		driver.findElement(By.cssSelector("a[href*='http://automationpractice.com/index.php?fc=module&module=bankwire&controller=payment']")).click();
+		driver.findElement(By.cssSelector(".cart_navigation .button")).click();
 	}
 	
 	@AfterTest
